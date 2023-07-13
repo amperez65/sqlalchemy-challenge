a3a82a85980134db1a66288d14698cb4d99991da
amperez65 <123400753+amperez65@users.noreply.github.com>
2023-07-13 00:31:06 -0500

    Add files via upload
    updated flask code (manually uploaded as I have overwritten clone locations)

--- amp_app/app.py
+++ amp_app/app.py
@@ -9,6 +9,11 @@ from flask import Flask, jsonify
 import json
 
 #################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
 # Database Setup
 #################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
@@ -24,10 +29,6 @@ engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # Create our session (link) from Python to the DB
 
 
-#################################################
-# Flask Setup
-#################################################
-app = Flask(__name__)
 
 
 
@@ -39,9 +40,12 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return(
-        f"""<h1>Available Routes:</h1><br/>"""
-        f"""/api/v1.0/precipitation<br/>"""
-        f"""/api/v1.0/passengers"""
+        f"""<h1>Available Routes:</h1><br/>
+        <a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a><br/>
+        <a href="/api/v1.0/stations">/api/v1.0/stations</a><br/>
+        <a href="/api/v1.0/tobs">/api/v1.0/tobs</a><br/>
+        <a href="/api/v1.0/2016-08-23">/api/v1.0/2016-08-23</a><br/>
+        <a href="/api/v1.0/2016-08-23/2016-09-23">/api/v1.0/2016-08-23/2016-09-23</a><br/>"""
 
     )
 
@@ -66,6 +70,103 @@ def precipitation():
     data = json.loads(df.to_json(orient="records"))
     return(data)
 
+@app.route("/api/v1.0/stations")
+def stations():
+    """Get stations"""
+    query = text("""
+                SELECT
+                    station,
+                    count(*) as num_obs
+                FROM
+                    measurement
+                group by
+                    station
+                order by
+                    num_obs desc;
+            """)
+
+    df = pd.read_sql(query, engine)
+
+    # convert to JSON
+    data = json.loads(df.to_json(orient="records"))
+    return(data)
+
+@app.route("/api/v1.0/tobs")
+def temperature():
+    """Get stations"""
+    query = text("""
+                SELECT
+                    station,
+                    date,
+                    tobs as temperature
+                FROM
+                    measurement
+                WHERE
+                    station = 'USC00519281'
+                    and date >= '2016-08-23'
+                order by
+                    date asc;
+
+            """)
+
+    df = pd.read_sql(query, engine)
+
+    # convert to JSON
+    data = json.loads(df.to_json(orient="records"))
+    return(data)
+
+@app.route("/api/v1.0/<start>")
+def temperature_start(start):
+    """Get stations"""
+    query = text(f"""
+                SELECT
+                    station,
+                    min(tobs) as tmin,
+                    avg(tobs) as tmean,
+                    max(tobs) as tmax
+                FROM
+                    measurement
+                WHERE
+                    date >= '{start}';
+
+            """)
+
+    df = pd.read_sql(query, engine)
+
+    # convert to JSON
+    data = json.loads(df.to_json(orient="records"))
+    return(data)
+
+@app.route("/api/v1.0/<start>/<end>")
+def temperature_start_end(start, end):
+    """Get stations"""
+    query = text(f"""
+                SELECT
+                    station,
+                    min(tobs) as tmin,
+                    avg(tobs) as tmean,
+                    max(tobs) as tmax
+                FROM
+                    measurement
+                WHERE
+                    date >= '{start}'
+                    and date <= '{end}';
+
+            """)
+
+    df = pd.read_sql(query, engine)
+
+    # convert to JSON
+    data = json.loads(df.to_json(orient="records"))
+    return(data)
+
+
+
+
+
+
+
+# website
 
 if __name__ == '__main__':
     app.run(debug=True)
